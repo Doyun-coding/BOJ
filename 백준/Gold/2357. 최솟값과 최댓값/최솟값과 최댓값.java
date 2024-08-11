a6fbecbcab08afe9ec3400aee02780cf3e498e15
doyun <90900630+Doyun-coding@users.noreply.github.com>
2024-08-11 13:22:07 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 760 ms, Memory: 77960 KB -BaekjoonHub

---
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw;
+    static int N, M;
+    static int[] arr;
+    static int[] minTree;
+    static int[] maxTree;
+
+    static int maxFind(int start, int end, int node, int left, int right) {
+        if(left > end || right < start ) {
+            return Integer.MIN_VALUE;
+        }
+
+        if(start >= left && end <= right) {
+            return maxTree[node];
+        }
+
+        int mid = (start + end) / 2;
+
+        return Math.max(maxFind(start, mid, node * 2, left, right), maxFind(mid + 1, end, node * 2 + 1, left, right));
+    }
+
+    static int minFind(int start, int end, int node, int left, int right) {
+        if(left > end || right < start) {
+            return Integer.MAX_VALUE;
+        }
+
+        if(start >= left && end <= right) {
+            return minTree[node];
+        }
+
+        int mid = (start + end) / 2;
+
+        return Math.min(minFind(start, mid, node * 2, left, right), minFind(mid + 1, end, node * 2 + 1, left, right));
+    }
+
+    static int maxInit(int start, int end, int node) {
+        if(start == end) {
+            return maxTree[node] = arr[start];
+        }
+
+        int mid = (start + end) / 2;
+
+        return maxTree[node] = Math.max(maxInit(start, mid, node * 2), maxInit(mid + 1, end, node * 2 + 1));
+    }
+
+    static int minInit(int start, int end, int node) {
+        if(start == end) {
+            return minTree[node] = arr[start];
+        }
+
+        int mid = (start + end) / 2;
+
+        return minTree[node] = Math.min(minInit(start, mid, node * 2), minInit(mid + 1, end, node * 2 + 1));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N+1];
+
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        minTree = new int[4*N];
+        maxTree = new int[4*N];
+
+        minInit(1, N, 1);
+        maxInit(1, N, 1);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+
+            sb.append(minFind(1, N, 1, left, right) + " " + maxFind(1, N, 1, left, right) + "\n");
+        }
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+    }
+}
