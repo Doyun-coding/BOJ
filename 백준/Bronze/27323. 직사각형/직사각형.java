fd328ae70e3d67d8ab0bc71913dfd850fa7ef57a
doyun <90900630+Doyun-coding@users.noreply.github.com>
2024-08-31 00:06:56 +0900

    [Bronze V] Title: 직사각형, Time: 112 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedWriter bw;
+    static int A, B;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        B = Integer.parseInt(st.nextToken());
+        int C = A * B;
+        bw.write(C + "\n");
+        bw.flush();
+        bw.close();
+    }
+    
+}
