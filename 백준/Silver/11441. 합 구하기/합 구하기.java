a2b0a2eba45a904d18660dbd95c24d717e57b1d0
doyun <90900630+Doyun-coding@users.noreply.github.com>
2024-09-08 23:19:43 +0900

    [Silver III] Title: 합 구하기, Time: 584 ms, Memory: 52284 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw;
+    static int N, M;
+    static int[] arr;
+    static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N+1];
+        dp = new long[N+1];
+
+        dp[0] = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = dp[i-1] + arr[i];
+        }
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            long sum = 0;
+            sum = dp[end] - dp[start-1];
+            bw.write(sum + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
