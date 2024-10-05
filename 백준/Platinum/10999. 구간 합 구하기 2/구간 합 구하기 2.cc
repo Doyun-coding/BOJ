aada342b1039515c2faba2a3acf2aa7ec2b6adb1
doyun <90900630+Doyun-coding@users.noreply.github.com>
2024-10-05 22:55:59 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 860 ms, Memory: 72368 KB -BaekjoonHub

---
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, K;
+long long arr[1000001];
+long long segmentTree[4000001];
+long long lazy[4000001];
+
+void updateLazy(int start, int end, int node) {
+    if(lazy[node] != 0) {
+        segmentTree[node] += ((long long)end - start + 1) * lazy[node];
+        if(start != end) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        } 
+        lazy[node] = 0;
+    }
+}
+
+long long initTree(int start, int end, int node) {
+    if(start == end) {
+        return segmentTree[node] = arr[start];
+    }
+
+    int mid = (start + end) / 2;
+
+    return segmentTree[node] = initTree(start, mid, node*2) + initTree(mid+1, end, node*2+1);
+}
+
+long long sumTree(int start, int end, int node, int left, int right) {
+    updateLazy(start, end, node);
+
+    if(left > end || right < start) {
+        return 0;
+    }
+
+    if(left <= start && end <= right) {
+        return segmentTree[node];
+    }
+
+    int mid = (start + end) / 2;
+
+    return sumTree(start, mid, node*2, left, right) + sumTree(mid+1, end, node*2+1, left, right);
+}
+
+void update(int start, int end, int node, int left, int right, long long diff) {
+    updateLazy(start, end, node);
+
+    if(right < start || end < left) {
+        return;
+    }
+
+    if(left <= start && end <= right) {
+        lazy[node] += diff;
+        updateLazy(start, end, node);
+
+        return;
+    }
+
+    int mid = (start + end) / 2;
+
+    update(start, mid, node * 2, left, right, diff);
+    update(mid+1, end, node * 2 + 1, left, right, diff);
+
+    segmentTree[node] = segmentTree[node * 2] + segmentTree[node * 2 + 1];
+}
+
+int main() {
+    cin >> N >> M >> K;
+
+
+    for(int i = 1; i <= N; i++) {
+        long long n;
+        cin >> n;
+        arr[i] = n;
+    }
+
+    initTree(1, N, 1);
+
+    for(int i = 1; i <= M+K; i++) {
+        int a;
+        cin >> a;
+
+        if(a == 1) {
+            int b, c;
+            long long d;
+            cin >> b >> c >> d;
+
+            update(1, N, 1, b, c, d);
+        }
+        else if(a == 2) {
+            int b, c;
+            cin >> b >> c;
+
+            long long sum = sumTree(1, N, 1, b, c);
+
+            cout << sum << "\n";
+        }
+    }
+
+    return 0;
+}
