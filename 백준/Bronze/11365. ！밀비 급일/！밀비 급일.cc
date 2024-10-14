5368158482f86b62320ed338f45f5896d29c1680
doyun <90900630+Doyun-coding@users.noreply.github.com>
2024-10-15 01:31:16 +0900

    [Bronze IV] Title: !밀비 급일, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+
+    while(1) {
+        getline(cin, s);
+
+        if(s == "END") break;
+
+        // for(int i = s.size()-1; i >= 0; i--) {
+        //     cout << s[i];
+        // }
+        // cout << "\n";
+        reverse(s.begin(), s.end());
+
+        cout << s << "\n";
+    }
+
+
+    return 0;
+}
