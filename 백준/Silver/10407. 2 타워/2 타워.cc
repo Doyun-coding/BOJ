6ae78663f8d1a5e2c73ecb0fe3ec34f6acc41914
doyun <90900630+Doyun-coding@users.noreply.github.com>
2024-10-31 19:08:38 +0900

    [Silver III] Title: 2 타워, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Silver/10407. 2 타워/2 타워.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long H;
+
+int main() {
+    cin >> H;
+
+    if(H == 1) {
+        cout << "2" << "\n";
+    }
+    else {
+        cout << "1" << "\n";
+    }
+
+    return 0;
+}
