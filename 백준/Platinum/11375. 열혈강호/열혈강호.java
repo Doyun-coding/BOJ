8ec0b6ea71e46a933927c0653a087e0bf576119c
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-01-11 16:42:39 +0900

    [Platinum IV] Title: 열혈강호, Time: 1100 ms, Memory: 103732 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw;
+    static int N, M;
+    static ArrayList<Integer>[] arrayLists;
+    static boolean[] check;
+    static int[] d;
+    static int cnt = 0;
+
+    static boolean DFS(int u) {
+        for(int n : arrayLists[u]) {
+            if(!check[n]) {
+                check[n] = true;
+
+                if(d[n] == 0 || DFS(d[n])) {
+                    d[n] = u;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M =Integer.parseInt(st.nextToken());
+        arrayLists = new ArrayList[N+1];
+        for(int i = 0; i <= N; i++) {
+            arrayLists[i] = new ArrayList<>();
+        }
+
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+
+            for(int j = 1; j <= k; j++) {
+                int n = Integer.parseInt(st.nextToken());
+
+                arrayLists[i].add(n);
+            }
+        }
+
+        check = new boolean[M+1];
+        d = new int[M+1];
+
+        for(int i = 1; i <= N; i++) {
+            Arrays.fill(check, false);
+            if(DFS(i)) {
+                cnt++;
+            }
+        }
+
+        bw.write(cnt + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
